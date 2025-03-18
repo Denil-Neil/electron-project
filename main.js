2ca5faa16d4d4f9b1f9528f160b90c9b8cf86a42
Denil Neil <denilcollins@gmail.com>
2025-03-17 22:57:29 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,49 @@
+const { app, BrowserWindow, globalShortcut } = require('electron');
+const path = require('path');
+
+// Change this to your deployed server URL once you have it
+const SERVER_URL = process.env.SERVER_URL || 'http://localhost:3000';
+
+let mainWindow = null;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 400,
+    height: 600,
+    frame: false,
+    transparent: true,
+    alwaysOnTop: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  mainWindow.loadURL(`${SERVER_URL}/receiver`);
+  // mainWindow.webContents.openDevTools(); // Uncomment for debugging
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  // Register Command+B (or Control+B) shortcut
+  globalShortcut.register('CommandOrControl+B', () => {
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();
+    } else {
+      mainWindow.show();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+}); 
---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+const path = require('path');
+
+app.use(express.static('public'));
+
+// Add CORS headers for the web interface
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.get('/sender', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'sender.html'));
+});
+
+app.get('/receiver', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'receiver.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('message', (msg) => {
+    io.emit('message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+}); 
